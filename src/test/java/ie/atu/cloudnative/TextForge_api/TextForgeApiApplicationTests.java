e546fe9aa76c283bab961a572d4e4e51c9cc2be0
Cian Smith <scian94@gmail.com>
2025-11-26 11:47:36 +0000

    Initial commit of Spring files.

---
+++ src/test/java/ie/atu/cloudnative/TextForge_api/TextForgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cloudnative.TextForge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextForgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
