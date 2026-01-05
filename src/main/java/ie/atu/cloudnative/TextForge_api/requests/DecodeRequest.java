d0026e3b6526d43703499673bfdc2f132e58ec33
cianmacg <scian94@gmail.com>
2026-01-05 18:09:23 +0000

    Updated application.properties to run port 8081. Updated Dockerfile.thin to reflect port change. Updated DecodeRequest.java ("NotBlank" is for strings, should have been "NotEmpty").

--- src/main/java/ie/atu/cloudnative/TextForge_api/requests/DecodeRequest.java
+++ src/main/java/ie/atu/cloudnative/TextForge_api/requests/DecodeRequest.java
@@ -1,10 +1,10 @@
 package ie.atu.cloudnative.TextForge_api.requests;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record DecodeRequest(
         @NotNull(message = "Integer array tokens is a required field.")
-        @NotBlank(message = "Integer array tokens must not be empty.")
+        @NotEmpty(message = "Integer array tokens must not be empty.")
         int[] tokens
 ) {}
